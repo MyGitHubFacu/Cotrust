153964b3a416df3d1b2b716fb6a34fac4fc3b3cf
OK <OK@DESKTOP-6LRKVMF>
2023-12-06 15:27:39 -0300

    .

--- Cotrust/Program.cs
+++ Cotrust/Program.cs
@@ -32,11 +32,11 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment() & !app.Environment.IsProduction())
 {
-    //app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    //app.UseHsts();
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
